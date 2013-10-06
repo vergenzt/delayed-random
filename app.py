9a0b7e04e0965365642dfb4cc94a8fd6fbb7ccb3
Tim Vergenz <vergenzt@gmail.com>
2013-10-06 11:21:16 -0400

    Use Gunicorn

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ def index():
     return 'Hello, world!'
 
 if __name__=='__main__':
-    port = int(os.environ.get('PORT', 5000))
-    bottle.run(server='gunicorn', host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 8080))
+    bottle.run(host='localhost', port=port)
+
+app = bottle.default_app()
 
